282322f9c21b19d4d5e2e0486168bfd7258098e1
Dlray89 <dlrayjr89@gmail.com>
2020-03-23 15:29:14 -0400

    starter depends add along with file and folder structure

---
+++ model/users.model.js
@@ -0,0 +1,22 @@
+
+
+module.exports = {
+    find,
+    findBy,
+    findById,
+    add,
+}
+
+function find(){
+
+}
+
+function findBy(){
+    
+}
+function findById(){
+    
+}
+function add(){
+    
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const cors = require("cors")
+
+
+// const userRouter = require("./router/users")
+const server = express()
+
+
+
+server.use(helmet())
+server.use(morgan("dev"))
+server.use(cors())
+// server.use("/api/users", userRouter)
+server.use(express.json())
+
+
+server.get("/", (req,res) => {
+    res.status(200).json({message: "Server is up and running"})
+})
+module.exports = server
